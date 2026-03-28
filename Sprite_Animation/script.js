794aea0852a94bf2ce40a3ac76feb55c4a3ac188
sunnyshah011 <sunnyshah35350@gmail.com>
2026-03-28 12:55:50 +0545

    new folder added

---
+++ Sprite_Animation/script.js
@@ -0,0 +1,103 @@
+let playerState = "run";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", function (e) {
+  playerState = e.target.value;
+});
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "shadow_dog.png";
+
+const spriteWidth = 575; // 6876/12 = 575px
+const spriteHeight = 523; // 5230/10 = 523px
+
+let gameFrame = 0;
+const staggerFrames = 5;
+
+const spritAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "gethit",
+    frames: 4,
+  },
+];
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spritAnimations[state.name] = frames;
+});
+
+function Animation() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  let position =
+    Math.floor(gameFrame / staggerFrames) %
+    spritAnimations[playerState].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spritAnimations[playerState].loc[position].y;
+
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight,
+  );
+
+  gameFrame++;
+
+  requestAnimationFrame(Animation);
+}
+Animation();
---
+++ pacman-game/src/App.jsx
@@ -0,0 +1,11 @@
+import CanvasGame from "./components/CanvasGame";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-black">
+      <CanvasGame />
+    </div>
+  );
+}
+
+export default App;
